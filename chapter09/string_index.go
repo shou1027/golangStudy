98a1847098673bbcbc734528832de337fe878686
TRACKFARM_MICHI\tfmic <longword871@gmail.com>
2024-04-05 00:15:44 +0900

    文字列操作

---
+++ chapter09/string_index.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Index("The Go Programing Language", "Go"))
+
+	fmt.Println(strings.Index("The Go Programing Language", "xxx"))
+}
